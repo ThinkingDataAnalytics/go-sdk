07b2de3db689ddc6ddeb30605cfaedeb863014fd
杨雄 <yangxiong@thinkingdata.cn>
2022-06-16 17:03:25 +0800

    Release: v1.6.2

--- thinkingdata/utils.go
+++ thinkingdata/utils.go
@@ -105,8 +105,9 @@ func checkPattern(name []byte) bool {
 }
 
 func parseTime(input []byte) string {
-	var re = regexp.MustCompile(`(((\d{4}-\d{2}-\d{2})T(\d{2}:\d{2}:\d{2})(?:\.(\d{3}))\d+)?)(Z|[\+-]\d{2}:\d{2})`)
-	var substitution = "$3 $4.$5"
+	var re = regexp.MustCompile(`((\d{4}-\d{2}-\d{2})T(\d{2}:\d{2}:\d{2})(?:\.(\d{3}))\d*)(Z|[\+-]\d{2}:\d{2})`)
+	var substitution = "$2 $3.$4"
+
 	for re.Match(input) {
 		input = re.ReplaceAll(input, []byte(substitution))
 	}
